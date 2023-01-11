36841b553bbfc6d49e8cdd1d4c7ba23bcea0b5f5
oyuerdene <teal.2022.oyuerdene@gamil.com>
2023-01-12 02:12:31 +0800

    .

--- src/components/todoList.js
+++ src/components/todoList.js
@@ -5,7 +5,6 @@ import Form from "react-bootstrap/Form";
 export function TodoList({
   todos,
   editingTexts,
-  handleKeyUp,
   cancelEditingText,
   updateEditingText,
   handleDoneChange,
@@ -20,68 +19,18 @@ export function TodoList({
           return (
             <div key={todo.id}>
               {editingTexts[todo.id] !== undefined ? (
-                <Card
-                  className="d-flex flex-row gap-3 mb-4"
-                  style={{ border: "none", width: "610px", height: "64px" }}
-                >
-                  <InputGroup size="lg">
-                    <Form.Control
-                      aria-describedby="inputGroup-sizing-sm"
-                      onChange={(e) => handleEditingText(todo.id, e)}
-                    />
-                  </InputGroup>
-                  <Button
-                    variant="outline-secondary mt-2 me-2 border-0"
-                    style={{ height: "35px" }}
-                    onClick={() => cancelEditingText(todo.id)}
-                  >
-                    Болих
-                  </Button>
-
-                  <Button
-                    variant="outline-secondary mt-2 me-2 border-0"
-                    style={{ height: "35px" }}
-                    onClick={() => updateEditingText(index1, todo.id)}
-                  >
-                    Хадгалах
-                  </Button>
-                </Card>
+                <>
+                  <EditingItem handleEditingText={handleEditingText} cancelEditingText={cancelEditingText} updateEditingText={updateEditingText} todo={todo} index1={index1}/>
+                </>
               ) : (
                 <div className="d-flex">
-                  <Card
-                    className="mb-4 d-flex flex-row col-lg-6 "
-                    style={{ width: "400px" }}
-                  >
-                    <input
-                      type="checkbox"
-                      onChange={() => handleDoneChange(todo.id)}
-                      style={{ marginLeft: "10px" }}
-                    />
-                    <Card.Body
-                      style={{
-                        textDecoration: todo.done ? "line-through" : "none",
-                        fontSize: "20px",
-                      }}
-                    >
-                      {todo.text}
-                    </Card.Body>
-                  </Card>
-                  {!todo.done && (
-                    <Button
-                      variant="outline-secondary mt-2 mx-3 border-0"
-                      style={{ height: "35px" }}
-                      onClick={() => editTodoInline(todo.id, index1)}
-                    >
-                      Засах
-                    </Button>
-                  )}
-                  <Button
-                    variant="outline-secondary mt-2 me-2 border-0"
-                    style={{ height: "35px" }}
-                    onClick={() => handleDelete(index1)}
-                  >
-                    Устгах
-                  </Button>
+                  <NormalItem
+                    handleDelete={handleDelete}
+                    handleDoneChange={handleDoneChange}
+                    editTodoInline={editTodoInline}
+                    todo={todo}
+                    index1={index1}
+                  />
                 </div>
               )}
             </div>
@@ -92,12 +41,83 @@ export function TodoList({
   );
 }
 
-// function NormalItem(){
-//     return(<>
-//     </>)
-// }
+function NormalItem({
+  handleDoneChange,
+  editTodoInline,
+  handleDelete,
+  todo,
+  index1,
+}) {
+  return (
+    <>
+      <Card
+        className="mb-4 d-flex flex-row col-lg-6 "
+        style={{ width: "400px" }}
+      >
+        <input
+          type="checkbox"
+          onChange={() => handleDoneChange(todo.id)}
+          style={{ marginLeft: "10px" }}
+        />
+        <Card.Body
+          style={{
+            textDecoration: todo.done ? "line-through" : "none",
+            fontSize: "20px",
+          }}
+        >
+          {todo.text}
+        </Card.Body>
+      </Card>
+      {!todo.done && (
+        <Button
+          variant="outline-secondary mt-2 mx-3 border-0"
+          style={{ height: "35px" }}
+          onClick={() => editTodoInline(todo.id, index1)}
+        >
+          Засах
+        </Button>
+      )}
+      <Button
+        variant="outline-secondary mt-2 me-2 border-0"
+        style={{ height: "35px" }}
+        onClick={() => handleDelete(index1)}
+      >
+        Устгах
+      </Button>
+    </>
+  );
+}
 
-// function EditingItem(){
-//     return(<>
-//     </>)
-// }
+function EditingItem({handleEditingText, cancelEditingText, updateEditingText, todo, index1}) {
+  return (
+    <>
+      <Card
+        className="d-flex flex-row gap-3 mb-4"
+        style={{ border: "none", width: "610px", height: "64px" }}
+      >
+        <InputGroup size="lg">
+          <Form.Control
+            aria-describedby="inputGroup-sizing-sm"
+            onChange={(e) => handleEditingText(todo.id, e)}
+            // style={{borderColor: "" ?  "red" : "black"}}
+          />
+        </InputGroup>
+        <Button
+          variant="outline-secondary mt-2 me-2 border-0"
+          style={{ height: "35px" }}
+          onClick={() => cancelEditingText(todo.id)}
+        >
+          Болих
+        </Button>
+
+        <Button
+          variant="outline-secondary mt-2 me-2 border-0"
+          style={{ height: "35px" }}
+          onClick={() => updateEditingText(index1, todo.id)}
+        >
+          Хадгалах
+        </Button>
+      </Card>
+    </>
+  );
+}
