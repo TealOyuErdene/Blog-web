6fa49fbb3328e35e4dcb507243cced08b1e915ca
oyuerdene <teal.oyuerdene@gmail.com>
2023-01-20 17:30:40 +0800

    .

--- src/components/admin/AdminApp.js
+++ src/components/admin/AdminApp.js
@@ -1,8 +1,6 @@
 import NavbarItem from "./navbar";
 import { Route, Routes } from "react-router-dom";
 import MainTodo from "./todoMain";
-// import { TodoNew } from "../todoNew";
-// import { TodoList } from "../todoList";
 export function AdminApp() {
   return (
     <>
