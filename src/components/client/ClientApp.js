daae61322ae0b93e809208862c98567ddaf61031
oyuerdene <teal.2022.oyuerdene@gamil.com>
2023-02-01 02:17:27 +0800

    .

--- src/components/client/ClientApp.js
+++ src/components/client/ClientApp.js
@@ -1,24 +1,25 @@
-import { NavbarMain } from "./navbar";
+import "./main.css";
 import { CKEditor } from "@ckeditor/ckeditor5-react";
 import ClassicEditor from "@ckeditor/ckeditor5-build-classic";
 import { useState } from "react";
-import { Addnews } from "./addNews";
+import { NavbarMain } from "./navbar";
+import { AddPost } from "./post";
 export function ClientApp() {
   const [text, setText] = useState("");
   return (
     <>
       <NavbarMain />
-      <CKEditor
+      <AddPost />
+      {/* <CKEditor
         editor={ClassicEditor}
         data={text}
         onChange={(event, editor) => {
           const data = editor.getData();
           setText(data);
         }}
-      />
+      /> */}
 
       <div dangerouslySetInnerHTML={{ __html: text }}></div>
-      <Addnews />
     </>
   );
 }
